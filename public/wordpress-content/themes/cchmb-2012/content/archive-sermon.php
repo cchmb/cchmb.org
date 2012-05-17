6fc83b641dedf59f7aae330ed83689ce7f11977a
Will Norris <will@willnorris.com>
2012-05-17 16:10:57 -0700

    update sermons and style for theme

--- public/wordpress-content/themes/cchmb-2012/content/archive-sermon.php
+++ public/wordpress-content/themes/cchmb-2012/content/archive-sermon.php
@@ -1,13 +1,13 @@
 <section id="latest">
   <h2>Latest Sermons</h2>
 <?php
-    $args = array('post_type' => 'sermon', 'posts_per_page' => 5);
+    $args = array('post_type' => 'sermon', 'posts_per_page' => 3);
     $loop = new WP_Query($args);
     while( $loop->have_posts() ) : $loop->the_post();
 ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       <?php if ( has_post_thumbnail() ): ?>
-        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a><br/>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a><br/>
       <?php endif; ?>
       <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </article>
@@ -19,7 +19,7 @@
   <?php
     $sermon_series = get_active_sermon_series();
     foreach($sermon_series as $series):
-      $series_thumbnail = get_the_sermon_series_thumbnail($series->term_id, 'thumbnail');
+      $series_thumbnail = get_the_sermon_series_thumbnail($series->term_id, 'medium');
   ?>
     <article>
       <?php if ( $series_thumbnail ): ?>
--- public/wordpress-content/themes/cchmb-2012/content/taxonomy-sermon_series.php
+++ public/wordpress-content/themes/cchmb-2012/content/taxonomy-sermon_series.php
@@ -3,19 +3,21 @@
 ?>
 
 <div class="series-thumbnail" style="float: left; margin-right: 1em;">
-  <?php the_sermon_series_thumbnail('thumbnail'); ?>
+  <?php the_sermon_series_thumbnail('medium'); ?>
 </div>
 <h1 class="page-title"><?php esc_html_e($series->name); ?></h1>
 <div class="archive-meta"><?php esc_html_e($series->description); ?></div>
 
 
-<h2><?php _e('Sermons', 'sermons'); ?></h2>
+<section style="clear: both;">
+  <h2><?php _e('Sermons', 'sermons'); ?></h2>
 
-<?php while ( have_posts() ) : the_post(); ?>
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php if ( has_post_thumbnail() ): ?>
-      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a><br/>
-    <?php endif; ?>
-    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-  </article>
-<?php endwhile; ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <?php if ( has_post_thumbnail() ): ?>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a><br/>
+      <?php endif; ?>
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </article>
+  <?php endwhile; ?>
+</section>
