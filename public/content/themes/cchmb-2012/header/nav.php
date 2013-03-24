721c41ae53178c62109b65f66a335958228ba8dc
Will Norris <will@willnorris.com>
2013-03-24 10:48:36 -0700

    update to latest pdx-theme

--- public/content/themes/cchmb-2012/header/nav.php
+++ public/content/themes/cchmb-2012/header/nav.php
@@ -1,5 +1,5 @@
-      <nav id="nav" class="main-nav" role="navigation">
-        <a href="#nav" id="menu-toggle"><?php _e('Menu', 'pdx'); ?></a>
+      <nav id="site-navigation" class="navigation-main" role="navigation">
+        <a href="#site-navigation" id="menu-toggle"><?php _e('Menu', 'pdx'); ?></a>
 
         <form id="searchform" method="get" action="/">
           <label for="s" class="assistive-text">Search</label>
@@ -7,5 +7,5 @@
           <input type="submit" id="searchsubmit" value="Search" />
         </form>
 
-        <?php wp_nav_menu( array( 'container' => '', 'theme_location' => 'primary' ) ); ?>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
       </nav>
