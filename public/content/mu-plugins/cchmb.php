42d4ddd1e6a1e4c76f603e6614d971bf51b904a8
Will Norris <will@willnorris.com>
2014-01-15 21:50:48 -0800

    only add placeholder images for sermons and series

--- public/content/mu-plugins/cchmb.php
+++ public/content/mu-plugins/cchmb.php
@@ -17,3 +17,11 @@ function safe_email($atts, $content=null) {
     return sprintf('<a href="mailto:%1$s">%1$s</a>', antispambot($content));
 }
 add_shortcode('safe_email', 'safe_email');
+
+add_filter( 'genesis_get_image', function( $output, $args, $id ) {
+  $type = get_post_type( $id );
+  if ( $output == '' && ( $type == 'mbsb_sermon' || $type == 'mbsb_series' ) ) {
+    $output = '<img src="http://placehold.it/1600x900" />';
+  }
+  return $output;
+}, 99, 3);
