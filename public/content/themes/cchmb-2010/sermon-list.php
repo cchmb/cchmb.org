1f5f62275990fae80b35cc628ec229958553b8c8
Will Norris <will@willnorris.com>
2014-02-08 13:58:35 -0800

    remove old unused themes and plugins

--- public/content/themes/cchmb-2010/sermon-list.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Template Name: Sermon List
- */
-?>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<?php the_content(); ?>
-<?php endwhile; // end of the loop. ?>
--- public/content/themes/cchmb-2012/header/title.php
+++
@@ -1,6 +0,0 @@
-      <hgroup>
-        <h1 class="site-title">
-          <a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr( get_bloginfo('name', 'display') ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="<?php esc_attr( get_bloginfo('name', 'display') ); ?>" /></a>
-        </h1>
-        <h2 class="site-description"><?php bloginfo('description'); ?></h2>
-      </hgroup>
