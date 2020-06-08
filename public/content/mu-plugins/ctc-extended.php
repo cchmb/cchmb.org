f367fba16e61c03513af5d375ec88cd1121da00e
Will Norris <will@willnorris.com>
2020-06-08 04:07:24 +0000

    simplify and rename ctc extended plugin

---
+++ public/content/mu-plugins/ctc-extended.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Extended Church Content
+Description: Extends Church Content plugin with additional functionality such as sermon fields for slides and Bible passage.
+Author: Will Norris
+Author URI: https://willnorris.com/
+*/
+
+add_filter( 'ctmb_fields-ctc_sermon_options', function( $fields ) {
+  $fields['_ctcx_sermon_slides'] = array(
+    'name'              => __( 'Slides', '' ),
+    'desc'              => __( 'Enter the URL to the sermon slides.', '' ),
+    'type'              => 'url',
+  );
+
+  $fields['_ctcx_sermon_passage'] = array(
+    'name'              => __( 'Passage', '' ),
+    'desc'              => __( 'Enter the primary bible passage(s).', '' ),
+    'type'              => 'text',
+  );
+
+  return $fields;
+}, 20);
