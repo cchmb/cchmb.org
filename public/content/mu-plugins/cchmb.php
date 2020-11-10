f2179d9936ed3a57122ed9f85567fb71ff324d93
Will Norris <will@willnorris.com>
2020-11-10 00:21:06 -0800

    cleanup mu-plugins/cchmb
    quite a bit of this is obsolete or moved into themes/cc14

--- public/content/mu-plugins/cchmb.php
+++ public/content/mu-plugins/cchmb.php
@@ -1,102 +1,39 @@
 <?php
 
-add_filter( 'genesis_footer_creds_text', 'cchmb_creds_text' );
-function cchmb_creds_text($text) {
-  $text     = sprintf( '[footer_copyright after=" %s"]', 'Calvary Chapel Half Moon Bay' );
-  return $text;
-}
-
-/**
- * Handle 'safe_email' shortcode which converts email address into spambot-safe
- * link.
- */
-function safe_email($atts, $content=null) {
-    return sprintf('<a href="mailto:%1$s">%1$s</a>', antispambot($content));
-}
-add_shortcode('safe_email', 'safe_email');
-
-add_filter( 'genesis_get_image', function( $output, $args, $id ) {
-  if ( empty($output) ) {
-    global $post;
-    $type = get_post_type( $post->ID );
-    switch ($type) {
-      case 'ctc_sermon':
-        $series = apply_filters('taxonomy-images-get-the-terms', '', ['post_id'=>$post->ID, 'taxonomy'=>'ctc_sermon_series']);
-        if ( $series && $series[0]->image_id ) {
-          $id = $series[0]->image_id;
-          if ( $id ) {
-            $html = wp_get_attachment_image($id, $args['size'], false, $args['attr']);
-            list( $url ) = wp_get_attachment_image_src($id, $args['size'], false, $args['attr']);
-          }
-        }
-      case 'mbsb_sermon':
-        // if a sermon doesn't have an image, use the image from the series
-        $series_id = get_post_meta($post->ID, 'series', true);
-        if ( has_post_thumbnail($series_id) ) {
-          $tn_id = get_post_thumbnail_id($series_id);
-
-          $html = wp_get_attachment_image($tn_id, $args['size'], false, $args['attr']);
-          list( $url ) = wp_get_attachment_image_src($tn_id, $args['size'], false, $args['attr']);
-        }
-        break;
-      case 'mbsb_series':
-        //$url = 'http://placehold.it/1600x900';
-        break;
-      case 'mbsb_preacher':
-        //$url = 'http://placehold.it/300x300';
-        break;
-    }
-
-    if ( 'html' === mb_strtolower($args['format']) ) {
-      $output = $html;
-    } else if ( 'url' === mb_strtolower($args['format']) ) {
-      $output = $url;
-    } else {
-      $output = str_replace( home_url(), '', $url );
-    }
-  }
-
-  return $output;
-}, 99, 3);
-
-add_filter( 'opengraph_image', function( $image ) {
-  if ( empty($image) && is_singular() && function_exists('genesis_get_image') ) {
-    $image = genesis_get_image('format=url');
-  }
-  return $image;
+// Handle [safe_email] shortcode which converts email address into spambot-safe link.
+add_shortcode('safe_email', function($atts, $content=null) {
+  return sprintf('<a href="mailto:%1$s">%1$s</a>', antispambot($content));
 });
 
+add_filter( 'opengraph_image', function( $image ) {
+  if ( function_exists('genesis_get_image') ) {
+    if ( $img = genesis_get_image('format=url') ) {
+      $image = $img;
+    }
+  }
+  return $image;
+}, 9);
+
 add_filter( 'opengraph_metadata', function( $metadata ) {
-  if ( is_singular() ) {
-    global $post;
-    $type = get_post_type( $post->ID );
-    switch ($type) {
-      case 'mbsb_sermon':
-        $metadata['og:type'] = 'article';
+  if ( is_singular('ctc_sermon') && function_exists('ctfw_sermon_data') ) {
+    extract( ctfw_sermon_data() );
+    $metadata['og:type'] = 'article';
 
-        $sermon = new mbsb_sermon($post->ID);
-
-        foreach ($sermon->attachments->get_attachments() as $k => $attachment) {
-            if (strstr($attachment->get_url(), 'youtube.com') !== false) {
-              parse_str( parse_url( $attachment->get_url(), PHP_URL_QUERY ) );
-              $metadata['og:type'] = 'video';
-              $metadata['og:video'] = 'https://www.youtube.com/v/' . $v . '?autohide=1&amp;version=3';
-              $metadata['og:video:type'] = 'application/x-shockwave-flash';
-              $metadata['og:video:width'] = '640';
-              $metadata['og:video:height'] = '360';
-            } else if (substr($attachment->get_mime_type(), 0, 5) == "audio") {
-              //$metadata['og:audio'] = $attachment->get_url();
-            }
-        }
-
-        break;
+    if (strstr($video, 'youtube.com') !== false) {
+      parse_str( parse_url( $video, PHP_URL_QUERY ) );
+      $metadata['og:type'] = 'video.other';
+      $metadata['og:video:url'] = 'https://www.youtube.com/embed/' . $v;
+      $metadata['og:video:secure_url'] = 'https://www.youtube.com/embed/' . $v;
+      $metadata['og:video:type'] = 'text/html';
+      $metadata['og:video:width'] = '1280';
+      $metadata['og:video:height'] = '720';
+    } else if ( $audio ) {
+      $metadata['og:audio:url'] = $audio;
+      $metadata['og:audio:type'] = 'audio/mpeg';
     }
   }
   return $metadata;
 });
-
-// Don't resize images inside of WordPress since Jetpack takes care of this for us.
-add_filter( 'intermediate_image_sizes_advanced', '__return_empty_array', 99 );
 
 // If hum is unable to resolve a shortcode, try to lookup by the "_original_post_id" post meta field.
 add_filter( 'hum_redirect_b', function($url, $id) {
